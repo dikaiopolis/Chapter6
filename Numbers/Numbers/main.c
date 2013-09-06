a597876106e76b316aeac23e81259d30b77e7f0d
Daniel Baldwin <daniel.j.baldwin@hotmail.com>
2013-09-06 14:41:48 -0500

    finished exercise files

---
+++ Numbers/Numbers/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Daniel Baldwin on 8/12/13.
+//  Copyright (c) 2013 Daniel Baldwin. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+    double y = 12345.6789;
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
+    printf("%.3f", sin(1));
+    return 0;
+}
+
